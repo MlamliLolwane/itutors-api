06060e62e05a0937c0b0fa1a876e6d92166b2532
MlamliLolwane <mrlolwane96@gmail.com>
2022-01-25 21:38:51 +0200

    Created University Module Seeder

--- database/seeders/UniversityModuleSeeder.php
+++ database/seeders/UniversityModuleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\UniversityModule;
 
 class UniversityModuleSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class UniversityModuleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        UniversityModule::create([
+            'module_code' => 'ONT1000',
+            'module_name' => 'Software Development 1',
+            'university' => 'Nelson Mandela University',
+        ]);
+
+        UniversityModule::create([
+            'module_code' => 'PRT2000',
+            'module_name' => 'Internet Programming 2',
+            'university' => 'Nelson Mandela University',
+        ]);
     }
 }
